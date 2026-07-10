0b4b9bf90bb2bdbd2e74eb1d01628fd140421dd2
Raphael Gruber <raphi011@gmail.com>
2026-07-10 10:28:52 +0200

    feat(gws): register Chat server widgets

--- src/modules/gws/server.ts
+++ src/modules/gws/server.ts
@@ -2,15 +2,25 @@ import "server-only";
 import { registerServerWidget } from "@/modules/server-registry";
 import {
   GMAIL_TYPE, CALENDAR_TYPE,
+  CHAT_DMS_TYPE, CHAT_CHANNELS_TYPE,
   gmailConfigSchema, gmailDefaultConfig,
   calendarConfigSchema, calendarDefaultConfig,
+  chatDmsConfigSchema, chatDmsDefaultConfig,
+  chatChannelsConfigSchema, chatChannelsDefaultConfig,
 } from "./manifest";
 import { fetchGmail } from "./gmail";
 import { fetchCalendar } from "./calendar";
+import { fetchChatDms, fetchChatChannels } from "./chat";
 
 registerServerWidget({
   type: GMAIL_TYPE, configSchema: gmailConfigSchema, defaultConfig: gmailDefaultConfig, fetch: fetchGmail,
 });
 registerServerWidget({
   type: CALENDAR_TYPE, configSchema: calendarConfigSchema, defaultConfig: calendarDefaultConfig, fetch: fetchCalendar,
+});
+registerServerWidget({
+  type: CHAT_DMS_TYPE, configSchema: chatDmsConfigSchema, defaultConfig: chatDmsDefaultConfig, fetch: fetchChatDms,
+});
+registerServerWidget({
+  type: CHAT_CHANNELS_TYPE, configSchema: chatChannelsConfigSchema, defaultConfig: chatChannelsDefaultConfig, fetch: fetchChatChannels,
 });
